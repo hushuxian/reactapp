8c0dfcad216400552a0254e5852310f7c2b2afd3
hushuxian <Fischfilet@126.com>
2019-11-01 17:29:09 +0800

    11.1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './caculator-component/App';
-//import List from './treediff/Diff';
-//import App from './treediff/App';
-//import App from './dom/Form'
-//import App from './dom/demo1'
-//import App from './dom/demo2'
-//import App from "./ES6/class.js"
-//import App from "./ES6/async.js"
-//import App from "./component/IconCom"
+
 //import App from "./component/InputNumber"
-//import App from "./component/Confirm"
-import App from "./component/Confirm/index2"
+import App from "./component/Confirm"
 import './index.css';
 
 /*ReactDOM.render(
